50386b6410e7f6d55eb570ba127edd639aacf010
ArturoRodriguez2611 <arodriguezr@cdmx.gob.mx>
2025-08-06 09:50:43 -0600

    feat(configuración de vite.config.js)

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,15 @@ import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+    server: {
+        host: 'portafolio-arturo.cdmx.gob.mx',
+        port: 5173,
+        https: false,
+    },
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ['resources/js/app.js', 'resources/scss/app.scss', 'resources/scss/reconocimiento.scss'
+        ],
             refresh: true,
         }),
         tailwindcss(),
