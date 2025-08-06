dfe52c0f40cfbb2ce01533babbd348f2ff780692
ArturoRodriguez2611 <arodriguezr@cdmx.gob.mx>
2025-08-06 09:47:41 -0600

    feat(routes): Modificación en archivo de rutas

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,18 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('portafolio');
+})->name('home');
+
+Route::get('/reconocimientos', function () {
+    return view('reconocimiento');
+})->name('reconocimientos');
+
+Route::get('/certificaciones', function () {
+    return view('certificacion');
+})->name('certificaciones');
+
+Route::get('/diseños', function () {
+    return view('diseño-digitales');
+})->name('diseños');
+
